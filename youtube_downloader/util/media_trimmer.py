61af0a9553d60d3900ed40bed72110cba4dc2ec6
szasz <szaszpetya@gmail.com>
2022-11-23 20:13:34 +0100

    Task1 - fixed background image relative path - code formatting in media trimmer

--- youtube_downloader/util/media_trimmer.py
+++ youtube_downloader/util/media_trimmer.py
@@ -20,13 +20,13 @@ def trim_media(input_file, start_seconds, end_seconds, mediatype):
     
         trimmed_file_input = trimmed_audio = (
             input_stream
-                .filter_("atrim", start=start_seconds, end=end_seconds)
+                .filter_("atrim", start = start_seconds, end = end_seconds)
                 .filter_("asetpts", PRESENTATION_TIMESTAMPS)
             )
         
         if MP4 == mediatype:
-            trimmed_video = input_stream.trim(start=start_seconds, end=end_seconds).setpts(PRESENTATION_TIMESTAMPS)
-            trimmed_file_input = ffmpeg.concat(trimmed_video, trimmed_audio, v=1, a=1)
+            trimmed_video = input_stream.trim(start = start_seconds, end = end_seconds).setpts(PRESENTATION_TIMESTAMPS)
+            trimmed_file_input = ffmpeg.concat(trimmed_video, trimmed_audio, v = 1, a = 1)
         
         ffmpeg.output(trimmed_file_input, trimmed_file_result, format=mediatype).run()
         
